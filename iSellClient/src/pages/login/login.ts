b96a15ce6732c695c0c3acad812270aa5f19245a
andry19 <andry2391@gmail.com>
2017-05-31 14:55:20 +0200

    login and pass hashing works as it should

--- iSellClient/src/pages/login/login.ts
+++ iSellClient/src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Http, RequestOptions } from '@angular/http';
 
 /**
  * Generated class for the LoginPage page.
@@ -14,7 +16,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+    this.loginForm = new FormGroup({
+      'username': new FormControl(),
+      'password': new FormControl()
+    });
+  }
+
+  login(){
+    let body = {username: this.loginForm.value.username, password: this.loginForm.value.password };
+    console.log(this.loginForm.value.username);
+    let url = 'http://127.0.0.1:3000/account/login';
+
+    this.http
+      .post(url, body)
+        .subscribe(data => {
+              console.log(data);
+        }, error => {
+            console.log(error.json());
+        });
   }
 
   ionViewDidLoad() {
